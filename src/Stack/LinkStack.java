04c60ceaaed7f55eaee41fdc901377b171d11ceb
krismile-sunflower <892212481@qq.com>
2020-10-20 17:23:34 +0800

    新增栈的测试

---
+++ src/Stack/LinkStack.java
@@ -0,0 +1,63 @@
+package Stack;
+
+import LinkedList.Node;
+
+public class LinkStack implements IStack{
+    private Node top;
+
+    @Override
+    public void clear() {
+        top = null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    @Override
+    public int length() {
+        Node p = top;
+        int length = 0;
+        while (p != null){
+            p = p.getNext();
+            ++length;
+        }
+        return length;
+    }
+
+    @Override
+    public Object peek() {
+        if(!isEmpty())
+            return top.getData();
+        else
+            return null;
+    }
+
+    @Override
+    public void push(Object x) throws Exception {
+        Node p = new Node(x);
+        p.setNext(top);
+        top = p;
+    }
+
+    @Override
+    public Object pop() {
+        if(isEmpty())
+            return null;
+        else {
+            Node p = top;
+            top = top.getNext();
+            return p.getData();
+        }
+    }
+
+    @Override
+    public void display() {
+        Node p = top;
+        while (p != null){
+            System.out.print(p.getData().toString() + " ");
+            p = p.getNext();
+        }
+    }
+}
---
+++ src/Stack/SqStack.java
@@ -0,0 +1,61 @@
+package Stack;
+
+public class SqStack implements IStack{
+    private Object[] stackElem;
+    private int top;
+
+    public SqStack(int maxSize) {
+        top = 0;
+        stackElem =new Object[maxSize];
+    }
+
+    @Override
+    public void clear() {
+        top = 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return top == 0;
+    }
+
+    @Override
+    public int length() {
+        return top;
+    }
+
+    @Override
+    public Object peek() {
+        if(isEmpty())
+            return null;
+        else
+            return stackElem[top - 1];
+    }
+
+    @Override
+    public void push(Object x) throws Exception {
+        if(top == stackElem.length)
+            throw new Exception("该栈已经满了");
+        else {
+            stackElem[top++] = x;
+        }
+
+    }
+
+    @Override
+    public Object pop() {
+        if(isEmpty())
+            return null;
+        else{
+            return stackElem[--top];
+        }
+    }
+
+    @Override
+    public void display() {
+        for(int i = top - 1; i >= 0; i--){
+            System.out.print(stackElem[i].toString() + " ");
+        }
+        System.out.println();
+    }
+}
